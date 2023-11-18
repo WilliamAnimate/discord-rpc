ff99902d4fbc35a4e3fc1c5df1a60faf10bb5ae5
William Animate <75321465+WilliamAnimate@users.noreply.github.com>
2023-11-18 14:45:06 -0500

    slightly better build (compile) script

--- c.bat
+++ c.bat
@@ -2,3 +2,5 @@
 
 cd src-tauri
 cargo build --target x86_64-pc-windows-msvc --profile idfk -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort
+cd target/x86_64-pc-windows-msvc/idfk
+start.
