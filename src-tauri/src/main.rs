9cc2291e077467dd62662b5899121a5dc7152968
William Animate <75321465+WilliamAnimate@users.noreply.github.com>
2023-11-18 14:28:53 -0500

    remove old goofy testing code that never worked

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -15,28 +15,7 @@ use console::show_console;
 lazy_static! {
     /*     oh my god rust is such a joke: the beginning of the saga     */
 
-    // static ref DETAILS: &'static str = "";
-
-    // static ref DETAILS: Mutex<String> = Mutex::new("hey guys".to_string());
-    // static ref STATE: Mutex<String> = Mutex::new("i refactored the entire application, if this is working then im gonna be so glad".to_string());
-    // static ref TIMESTAMP_ENABLED: Mutex<bool> = Mutex::new(false);
-    // static ref BUTTON_ONE_TEXT: Mutex<String> = Mutex::new("test".to_string());
-    // static ref BUTTON_ONE_LINK: Mutex<String> = Mutex::new("https://github.com/williamanimate".to_string());
-    // static ref BUTTON_TWO_TEXT: Mutex<String> = Mutex::new("test2".to_string());
-    // static ref BUTTON_TWO_LINK: Mutex<String> = Mutex::new("https://therickroll.com".to_string());
-    // static ref LARGE_IMAGE_ASSET_NAME: Mutex<String> = Mutex::new("niko".to_string());
-    // static ref LARGE_IMAGE_ASSET_TEXT: Mutex<String> = Mutex::new("niko oneshot".to_string());
-    // static ref SMALL_IMAGE_ASSET_NAME: Mutex<String> = Mutex::new("niko".to_string());
-    // static ref SMALL_IMAGE_ASSET_TEXT: Mutex<String> = Mutex::new("niko oneshot".to_string());
-
-    // static ref CLIENT: Mutex<DiscordIpcClient> = Mutex::new(DiscordIpcClient::new("1173464188787846165"));
-
     static ref CLIENT_ID: Mutex<String> = Mutex::new("".to_string());
-
-    // N.B. this code doesn't work because the stupid process down there acquires the lock for this thing and doesn't let it edit
-    // so we're using static mut, i don't know if it'll be memory safe, but it *should* be.
-    // 💀💀💀
-    // static ref MUTLITHREAD_ASSIST_REQUEST_STOP_RPC: Mutex<bool> = Mutex::new(false);
 
     static ref DETAILS: Mutex<String> = Mutex::new("".to_string());
     static ref STATE: Mutex<String> = Mutex::new("".to_string());
@@ -50,13 +29,11 @@ lazy_static! {
     static ref SMALL_IMAGE_ASSET_NAME: Mutex<String> = Mutex::new("".to_string());
     static ref SMALL_IMAGE_ASSET_TEXT: Mutex<String> = Mutex::new("".to_string());
 
-    // static ref TEST: Mutex<bool> = Mutex::new(false);
     static ref RPC_UP: Mutex<bool> = Mutex::new(false);
 }
 
 // ohshit unsafe rust 💀💀
-// i just gave up trying to use lazy_static lmaooo
-// static mut CLIENT: Result<DiscordIpcClient, Box<dyn std::error::Error>> = DiscordIpcClient::new("1173464118787846165");
+// gave up using lazy_static
 static mut MULTITHREAD_ASSIST_REQUEST_STOP_RPC: bool = false;
 
 #[tauri::command]
@@ -147,63 +124,7 @@ fn set_small_image_asset_text(x: &str) {
     *y = x.to_string();
 }
 
-// fn set_(x: &str) {
-//     let mut d = A.lock().unwrap();
-//     *d = x.to_string();
-// }
-
-fn _test_values_or_something_idfk() {
-    set_details("detail");
-    set_state("state");
-    set_timestamp_enabled(true);
-    set_button_one_text("btn1 txt");
-    set_button_one_link("https://example.com/btn1_txt");
-    set_button_two_text("btn2 txt");
-    set_button_two_link("https://example.com/btn1_txt");
-    set_large_image_asset_name("niko");
-    set_large_image_asset_text("large image asset text");
-    set_small_image_asset_name("niko");
-    set_small_image_asset_text("small image asset text");
-// // this comment serves no purpose other than to link everything so i can collapse it in one click.
-//     let details = DETAILS.lock().unwrap();
-//     let state = STATE.lock().unwrap();
-//     let timestamp_enabled = TIMESTAMP_ENABLED.lock().unwrap();
-//     let button_one_text = BUTTON_ONE_TEXT.lock().unwrap();
-//     let button_one_link = BUTTON_ONE_LINK.lock().unwrap();
-//     let button_two_text = BUTTON_TWO_TEXT.lock().unwrap();
-//     let button_two_link = BUTTON_TWO_LINK.lock().unwrap();
-//     let large_image_asset_name = LARGE_IMAGE_ASSET_NAME.lock().unwrap();
-//     let large_image_asset_text = LARGE_IMAGE_ASSET_TEXT.lock().unwrap();
-//     let small_image_asset_name = SMALL_IMAGE_ASSET_NAME.lock().unwrap();
-//     let small_image_asset_text = SMALL_IMAGE_ASSET_TEXT.lock().unwrap();
-// // this comment serves no purpose other than to link everything so i can collapse it in one click.
-//     dbg!(details);
-//     dbg!(state);
-//     dbg!(timestamp_enabled);
-//     dbg!(button_one_text);
-//     dbg!(button_one_link);
-//     dbg!(button_two_text);
-//     dbg!(button_two_link);
-//     dbg!(large_image_asset_name);
-//     dbg!(large_image_asset_text);
-//     dbg!(small_image_asset_name);
-//     dbg!(small_image_asset_text);
-}
-
 fn main() {
-    //
-    // this code's only purpose is to make sure if setting the variables actually work
-    // in this case, it did.
-    // {
-    //     let mut state = STATE.lock().unwrap();
-    //     println!("initial: {}", state);
-    //     *state = "a".to_string();
-    //     println!("post edit test 1: {}", state);
-    //     // rich_presence_callback();
-    // }
-    // let mut state = STATE.lock().unwrap();
-    // println!("post edit test 2: {}", state);
-
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             dbg_show_console,
@@ -234,10 +155,7 @@ fn rich_presence_callback() {
     println!("{:?}", set_rich_presence());
 }
 
-// fn set_rich_presence() -> Result<(), Box<dyn std::error::Error>> {
 fn set_rich_presence() -> Result<(), Box<dyn std::error::Error>> {
-// fn set_rich_presence() {
-// fn set_rich_presence(client_id: &str) {
     std::thread::spawn(|| {
         println!("starting rich presence");
         /*     oh my god rust is such a joke: the saga continues     */
@@ -255,14 +173,8 @@ fn set_rich_presence() -> Result<(), Box<dyn std::error::Error>> {
         let small_image_asset_name = SMALL_IMAGE_ASSET_NAME.lock().unwrap();
         let small_image_asset_text = SMALL_IMAGE_ASSET_TEXT.lock().unwrap();
 
-        // let mut client = DiscordIpcClient::new("1173464118787846165").expect("wtf");
         let mut client = DiscordIpcClient::new(&client_id).expect("wtf");
-        // let mut client = DiscordIpcClient::new("1173464118787846165")?;
 
-        // let mut client_ref = client.as_ref();
-        // client_ref = client_ref.expect("wtf").connect();
-
-        // i dont even know what rust wants from me or my family anymore, wtf?????
         let _ = client.connect();
 
         let mut activity_base = activity::Activity::new();
@@ -286,18 +198,6 @@ fn set_rich_presence() -> Result<(), Box<dyn std::error::Error>> {
             println!("timestamp enabled");
             activity_base = activity_base.timestamps(activity::Timestamps::new().start(std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs() as i64));
         }
-
-        // if *button_one_text != "" {
-        //     if *button_one_link != "" {
-        //         activity_base = activity_base.buttons(vec![activity::Button::new(&button_one_text, &button_one_link)])
-        //     }
-        // }
-
-        // if *button_two_text != "" {
-        //     if *button_two_link != "" {
-        //         activity_base = activity_base.buttons(vec![activity::Button::new(&button_two_text, &button_two_link)])
-        //     }
-        // }
 
         if *button_one_text != "" {
             if *button_one_link != "" {
@@ -355,8 +255,6 @@ fn set_rich_presence() -> Result<(), Box<dyn std::error::Error>> {
         let _ = client.set_activity(activity_base.assets(activity_assets));
 
         loop {
-            // let stop_req = MUTLITHREAD_ASSIST_REQUEST_STOP_RPC.lock().unwrap();
-            // println!("{}", &stop_req);
             unsafe {
                 // this is multithreaded, might cause memory leaks 💀
                 if MULTITHREAD_ASSIST_REQUEST_STOP_RPC {
